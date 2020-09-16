0515eed6cd2478fb352894d40790832662df7114
Jolgo <jlgo60@hotmail.com>
2020-09-16 18:26:54 -0500

    Se agrego selectedItem

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,33 +1,55 @@
 package com.example.myapplication
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 //Fuente : https://dzone.com/articles/custom-spinner-for-android-application
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        spinner.adapter = MoodArrayAdapter(
-            this,
-            listOf(
-
-                Mood(resources.getColor(R.color.blue, null), "Color1"),
-                Mood(resources.getColor(R.color.purple, null), "Color2"),
-                Mood(resources.getColor(R.color.green, null), "Color3"),
-                Mood(resources.getColor(R.color.darkred, null), "Color4")
-            //Si se trabaja con Fragment agregar Mood(context.resources.getColor(R.color.blue,null), "Angry")
-
-
-            )
+        val lista = listOf<Mood>(
+            Mood(resources.getColor(R.color.blue, null), "Color1"),
+            Mood(resources.getColor(R.color.purple, null), "Color2"),
+            Mood(resources.getColor(R.color.green, null), "Color3"),
+            Mood(resources.getColor(R.color.darkred, null), "Color4")
         )
 
+        val adaptador = MoodArrayAdapter(this, lista)
+        spinner1.adapter = adaptador
+
+        spinner1.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+                val selectedItem = (p0?.selectedItem as Mood)
+                if (selectedItem.description == "Color1") {
+                    textView.setBackgroundResource(R.color.green)
+                } else
+                    if (selectedItem.description == "Color2") {
+                        textView.setBackgroundResource(R.color.purple)
+                    }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
 
 
 }
+
+
+
+
+
 
 
