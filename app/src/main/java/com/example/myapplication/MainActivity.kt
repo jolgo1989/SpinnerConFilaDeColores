b9650eb2c2a47d30f0eed1add684ff18321b86c4
Jolgo <jlgo60@hotmail.com>
2020-09-18 06:35:11 -0500

    Proyecto finalizado

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,13 +9,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 //Fuente : https://dzone.com/articles/custom-spinner-for-android-application
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val lista = listOf<Mood>(
+            Mood(resources.getColor(R.color.white, null), "Seleccione un Color"),
             Mood(resources.getColor(R.color.blue, null), "Color1"),
             Mood(resources.getColor(R.color.purple, null), "Color2"),
             Mood(resources.getColor(R.color.green, null), "Color3"),
@@ -29,23 +28,30 @@ class MainActivity : AppCompatActivity() {
             AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
 
+//Metodo para seleccionar un valor
                 val selectedItem = (p0?.selectedItem as Mood)
-                if (selectedItem.description == "Color1") {
-                    textView.setBackgroundResource(R.color.green)
-                } else
-                    if (selectedItem.description == "Color2") {
-                        textView.setBackgroundResource(R.color.purple)
-                    }
+                when (selectedItem.description) {
+                    "Color1" -> textView.setBackgroundResource(R.color.blue)
+                    "Color2" -> textView.setBackgroundResource(R.color.purple)
+                    "Color3" -> textView.setBackgroundResource(R.color.green)
+                    "Color4" -> textView.setBackgroundResource(R.color.darkred)
+                    else-> textView.setBackgroundResource(R.color.white)
+                }
+
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
                 TODO("Not yet implemented")
             }
         }
+
     }
 
-
 }
+
+
+
+
 
 
 
